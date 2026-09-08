b8b7e9e545513a062f2cd8c650e766e1908f894d
Nursultan <nurrs.serkul@gmail.com>
2026-09-08 14:46:39 +0500

    method

--- base.cs
+++ base.cs
@@ -1,13 +1,43 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Квадрат: " + Square(9));
+        string status = "";
+        bool f = isPrime(17);
+        if (f){
+            status = "true";
+        }else{
+            status = "false";
+        }
+        Console.WriteLine("17: " + status);
+        bool s = isPrime(18);
+        if (s){
+            status = "true";
+        }else{
+            status = "false";
+        }
+        Console.WriteLine("18: " + status);
     }
 
-    static int Square(int x)
-{
-    return x * x;
-}
+    static bool isPrime(int n){
+        int x = 0;
+        int down = n;
+        while(down > 0){
+            if(n % down == 0){
+                x++;
+            }
+            down--;
+        }
+        if(x > 2){
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 }
 
