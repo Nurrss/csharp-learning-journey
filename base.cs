0bb62a4968a02da0c8511b70ac404ef516bfe307
Nursultan <nurrs.serkul@gmail.com>
2026-09-08 15:04:07 +0500

    method count vowels

--- base.cs
+++ base.cs
@@ -1,43 +1,19 @@
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-
 class Program
 {
     static void Main()
     {
-        string status = "";
-        bool f = isPrime(17);
-        if (f){
-            status = "true";
-        }else{
-            status = "false";
-        }
-        Console.WriteLine("17: " + status);
-        bool s = isPrime(18);
-        if (s){
-            status = "true";
-        }else{
-            status = "false";
-        }
-        Console.WriteLine("18: " + status);
+       Console.WriteLine("Гласных: " + CountVowels("Привет, как дела?"));
     }
 
-    static bool isPrime(int n){
-        int x = 0;
-        int down = n;
-        while(down > 0){
-            if(n % down == 0){
-                x++;
+    static int CountVowels(string text){
+        int sum = 0;
+        char[] letters = {'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'};
+        for(int i = 0; i < text.Length; i++){
+            if(letters.Contains(text[i])){
+                sum++;
             }
-            down--;
         }
-        if(x > 2){
-            return false;
-        }
-        else
-        {
-            return true;
-        }
+        return sum;
     }
 }
 
